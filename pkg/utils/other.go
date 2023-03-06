34d4ccb676bf04093f7e2a04f8ae59b771356b84
heqian <182359041@qq.com>
2023-03-06 23:00:28 +0800

    优化SQL

--- pkg/utils/other.go
+++ pkg/utils/other.go
@@ -3,7 +3,6 @@ package utl
 import (
 	"crypto/md5"
 	"encoding/hex"
-	"fmt"
 	"math/rand"
 	"reflect"
 	"strconv"
@@ -63,45 +62,4 @@ func ToQuestions[T any](l []T, question string) string {
 
 func ToPointer[T any](l T) *T {
 	return &l
-}
-
-const (
-	textBlack = iota + 30
-	textRed
-	textGreen
-	textYellow
-	textBlue
-	textPurple
-	textCyan
-	textWhite
-)
-
-func Black(str string) string {
-	return textColor(textBlack, str)
-}
-
-func Red(str string) string {
-	return textColor(textRed, str)
-}
-func Yellow(str string) string {
-	return textColor(textYellow, str)
-}
-func Green(str string) string {
-	return textColor(textGreen, str)
-}
-func Cyan(str string) string {
-	return textColor(textCyan, str)
-}
-func Blue(str string) string {
-	return textColor(textBlue, str)
-}
-func Purple(str string) string {
-	return textColor(textPurple, str)
-}
-func White(str string) string {
-	return textColor(textWhite, str)
-}
-
-func textColor(color int, str string) string {
-	return fmt.Sprintf("\x1b[0;%dm%s\x1b[0m", color, str)
 }
