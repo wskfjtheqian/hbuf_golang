347d911fa66d0ccd92aeb541d2448755c84d5d68
heqian <182359041@qq.com>
2023-05-31 09:51:17 +0800

    添加DB Json转换器

---
+++ pkg/db/converter.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+)
+
+type ConverterJson struct {
+	data any
+}
+
+func (t *ConverterJson) Scan(value any) error {
+	switch v := value.(type) {
+	case []byte:
+		err := json.Unmarshal(v, t.data)
+		if err != nil {
+			return err
+		}
+	case string:
+		err := json.Unmarshal([]byte(v), t.data)
+		if err != nil {
+			return err
+		}
+	}
+	return fmt.Errorf("Can't convert %T to DbToJson", value)
+}
+
+func (t ConverterJson) Value() (driver.Value, error) {
+	marshal, err := json.Marshal(t.data)
+	if err != nil {
+		return nil, err
+	}
+	return marshal, nil
+}
+
+func NewJson(data any) *ConverterJson {
+	return &ConverterJson{
+		data: data,
+	}
+}
