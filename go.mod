41954a3d3996da0c2fa811185db595cea01b0039
call <call@qq.com>
2024-12-31 18:09:07 +0800

    11

--- go.mod
+++ go.mod
@@ -2,6 +2,14 @@ module github.com/wskfjtheqian/hbuf_golang
 
 go 1.18
 
-require golang.org/x/net v0.33.0
+require (
+	github.com/gobwas/ws v1.4.0
+	github.com/gorilla/websocket v1.5.3
+	golang.org/x/net v0.33.0
+)
 
-require golang.org/x/text v0.21.0 // indirect
+require (
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	golang.org/x/text v0.21.0 // indirect
+)
--- pkg/rpc/websockt.go
+++ pkg/rpc/websockt.go
@@ -1,11 +1,35 @@
 package rpc
 
-import "golang.org/x/net/websocket"
+import (
+	"github.com/gobwas/ws"
+	"net"
+)
 
 type WebSocket struct {
-	conn *websocket.Conn
+	conn net.Conn
 }
 
 func (ws *WebSocket) run() {
 
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func NewWebSocketClient(baseURL string) *WebSocketClient {
+
+	return &WebSocketClient{}
+}
+
+type WebSocketClient struct {
+	conn   *ws.Dialer
+	filter ResponseFilter
+}
+
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+func NewWebSocketServer(addr string) *WebSocketServer {
+	return &WebSocketServer{}
+}
+
+type WebSocketServer struct {
+	listener net.Listener
 }
